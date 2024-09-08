e823f2ec69dc8fbca34e482e395e4c68f593225a
Tushar Bahuguna <tusharbahuguna583@gmail.com>
2024-09-08 13:57:21 +0530

    Added GPT movie Suggestion feature

--- src/components/GptSearchBar.js
+++ src/components/GptSearchBar.js
@@ -1,11 +1,26 @@
 import openai from "../utils/openai";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import lang from "../utils/languageConstants";
 import { useRef } from "react";
+import { API_OPTIONS } from "../utils/constants";
+import { addGptMovieResult } from "../utils/gptSlice";
 
 const GptSearchBar = () => {
+  const dispatch = useDispatch();
   const langKey = useSelector((store) => store.config.lang);
   const searchText = useRef(null);
+
+  const searchMovieTMDB = async (movie) => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/search/movie?query=" +
+        { movie } +
+        "&include_adult=false&language=en-US&page=1",
+      API_OPTIONS
+    );
+    const json = await data.json();
+
+    return json.results();
+  };
 
   const handleGptSearchClick = async () => {
     const gptQuery =
@@ -21,7 +36,15 @@ const GptSearchBar = () => {
     if (!gptResults.choices) {
       // display error message
     }
-    const getMovies = gptResults.choices?.[0]?.message?.content.split(",");
+    const gptMovies = gptResults.choices?.[0]?.message?.content.split(",");
+
+    const promiseArray = gptMovies.map((movie) => searchMovieTMDB(movie));
+
+    const tmdbResults = await Promise.all(promiseArray);
+
+    dispatch(
+      addGptMovieResult({ movieNames: gptMovies, movieResults: tmdbResults })
+    );
   };
 
   return (
